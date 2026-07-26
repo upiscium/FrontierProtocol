0b95c92baacf938e2dc89be017276900df9a4aa8
uPiscium <upiscium@gmail.com>
2026-07-26 20:21:54 +0900

    feat: render stabilizer chunk ranges

--- src/main/java/dev/upiscium/frontierprotocol/client/FrontierProtocolClientEvents.java
+++ src/main/java/dev/upiscium/frontierprotocol/client/FrontierProtocolClientEvents.java
@@ -1,9 +1,11 @@
 package dev.upiscium.frontierprotocol.client;
 
 import dev.upiscium.frontierprotocol.client.tooltip.FrontierProtocolItemTooltips;
+import dev.upiscium.frontierprotocol.client.render.StabilizerRangeRenderer;
 import net.minecraft.client.gui.screens.Screen;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.event.entity.player.ItemTooltipEvent;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
 
 public final class FrontierProtocolClientEvents {
     private FrontierProtocolClientEvents() {}
@@ -11,5 +13,10 @@ public final class FrontierProtocolClientEvents {
     @SubscribeEvent
     public static void itemTooltip(ItemTooltipEvent event) {
         FrontierProtocolItemTooltips.append(event.getItemStack(), event.getToolTip(), Screen.hasShiftDown());
+    }
+
+    @SubscribeEvent
+    public static void renderLevel(RenderLevelStageEvent event) {
+        StabilizerRangeRenderer.render(event);
     }
 }
